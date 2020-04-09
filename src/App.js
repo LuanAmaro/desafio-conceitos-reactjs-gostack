3e926ba907df01f6cbed347bd94f48e67da015fb
Luan Amaro <luan@tecnobit.com.br>
2020-04-09 12:38:17 -0400

    Challenge three  completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repository, setRepository] = useState([]);
+
+  useEffect(() => { 
+    async function loadRepository(){
+      const reponse = await api.get("repositories");
+      setRepository(reponse.data);
+    }
+    loadRepository();
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const repositorio = {
+      title: `Desafio 03: Conceitos do ReactJS - ${Date.now()}`,
+      url: "https://github.com/LuanAmaro/desafio-conceitos-node-gostack",
+      techs: ["NodeJS", "JavaScript"]
+    };
+
+    try {
+      const { data } = await api.post("repositories", repositorio);
+      setRepository([...repository, data]);
+    } catch (error) {
+      alert('Erro ao adicionar um novo repositorio, tente novamente.');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+      setRepository(repository.filter(rep => rep.id !== id));
+    } catch (error) {
+      alert('Erro ao deletar repositorio, tente novamente.');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repository.map((rep) =>
+            <li key={rep.id}>
+              {rep.title}
+              <button onClick={() => handleRemoveRepository(rep.id)}>
+                  Remover
+              </button>
+            </li>
+        )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
